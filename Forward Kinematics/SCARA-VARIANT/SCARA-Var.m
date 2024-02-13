b62583cbc264efc645ed2d70203cd8831e425669
Dannielle Louis Abeleda <157472473+icecreamperson@users.noreply.github.com>
2024-02-13 21:16:40 +0800

    Update SCARA-Var.m

--- Forward Kinematics/SCARA-VARIANT/SCARA-Var.m
+++ Forward Kinematics/SCARA-VARIANT/SCARA-Var.m
@@ -1,13 +1,13 @@
 disp('SCARA_VARIANT')
 syms a1 a2 a3 a4
 
-a1 = 20;
+a1 = 5;
 a2 = 5;
 a3 = 6;
 a4 = 5;
 
-H0_1 = Link([0,a1,a2,0,1,0]);
-H0_1.qlim = [10 30];
+H0_1 = Link([0,0,a2,0,1,a1]);
+H0_1.qlim = [0 5];
 
 H1_2 = Link([0,0,a3,0,0]);
 H1_2.qlim = pi/180*[-90 90];
