1d23e6ec883cb62aa184c4a1131463cbe0b41d4d
Dannielle Louis Abeleda <157472473+icecreamperson@users.noreply.github.com>
2024-02-13 20:42:29 +0800

    Add files via upload

---
+++ Forward Kinematics/SCARA-VARIANT/SCARA-Var.m
@@ -0,0 +1,20 @@
+disp('SCARA_VARIANT')
+syms a1 a2 a3 a4
+
+a1 = 20;
+a2 = 5;
+a3 = 6;
+a4 = 5;
+
+H0_1 = Link([0,a1,a2,0,1,0]);
+H0_1.qlim = [10 30];
+
+H1_2 = Link([0,0,a3,0,0]);
+H1_2.qlim = pi/180*[-90 90];
+
+H2_3 = Link([0,0,a4,0,0]);
+H2_3.qlim = pi/180*[-90 90];
+
+SCARA_VARIANT = SerialLink([H0_1 H1_2 H2_3], 'name', 'SCARA_VARIANT')
+SCARA_VARIANT.plot([0 0 0], 'workspace', [-5 30 -30 30 0 30])
+SCARA_VARIANT.teach
