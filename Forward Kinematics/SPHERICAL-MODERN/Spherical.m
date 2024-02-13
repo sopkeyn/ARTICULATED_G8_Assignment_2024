f8eddf9d71191fe613cf63887a3e3153b8efdbce
Dannielle Louis Abeleda <157472473+icecreamperson@users.noreply.github.com>
2024-02-13 21:33:15 +0800

    Update Spherical.m

--- Forward Kinematics/SPHERICAL-MODERN/Spherical.m
+++ Forward Kinematics/SPHERICAL-MODERN/Spherical.m
@@ -1,19 +1,19 @@
 disp('SPHERICAL_MODERN')
 syms a1 a2 a3
 
-a1 = 12;
-a2 = 6;
-a3 = 8;
+a1 = 2;
+a2 = 2;
+a3 = 2;
 
 H0_1 = Link([0,a1,0,pi/2,0,0]);
 H0_1.qlim = pi/180*[-90 90];
 
-H1_2 = Link([3*pi/2,0,a2,pi/2,0]);
+H1_2 = Link([pi/2,0,a2,pi/2,0]);
 H1_2.qlim = pi/180*[-90 90];
 
-H2_3 = Link([0,a3,0,0,0]);
-H2_3.qlim = pi/180*[-90 90];
+H2_3 = Link([0,0,0,0,1,a3]);
+H2_3.qlim = [0 3];
 
 SPHERICAL_MODERN = SerialLink([H0_1 H1_2 H2_3], 'name', 'SPHERICAL_MODERN')
-SPHERICAL_MODERN.plot([0 0 0 ], 'workspace', [-5 30 -30 30 0 30])
+SPHERICAL_MODERN.plot([0 0 0 ], 'workspace', [-10 10 -10 10 -5 10])
 SPHERICAL_MODERN.teach
